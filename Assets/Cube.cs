02f31a78ef57f80130324044de4e6c81dde72ebf
raracat <peapeapea5@gmail.com>
2014-10-18 14:02:14 +0900

    Cube.jsを作成

---
+++ Assets/Cube.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello,Goodbye");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
