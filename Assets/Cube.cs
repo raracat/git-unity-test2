01e2c67f6e37c590b2e7a8baf2063027ad268052
gogoty <gogoty.gogoty.gogoty@gmail.com>
2014-10-18 14:35:02 +0900

    Hello!raracat
    UnityでGITを使用するためのテスト
    raracatに挨拶

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -5,7 +5,7 @@ public class Cube : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Hello");
+		Debug.Log ("Hello!raracat!");
 	}
 	
 	// Update is called once per frame
